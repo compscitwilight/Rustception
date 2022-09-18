5a6e9bbdf6d048ed7829f404d6ed1cf5ec1adcbe
devrusty <rguillory246@gmail.com>
2022-09-18 16:16:41 -0500

    created rust

---
+++ src/rust.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Rustception {
+    public static class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("rust");
+        }
+    }
+}
